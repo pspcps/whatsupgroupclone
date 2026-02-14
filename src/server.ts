4e6213239516fa7082158f2d4dc868091f0e3373
Prakash <prakash.seervi9460@gmail.com>
2026-02-14 13:26:59 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,10 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-// import serverless from "serverless-http"
+// src/server.js
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("WORKING"));
+module.exports = app;
 
-// import groupRoutes from "./routes/group.routes"
-// import { errorHandler } from "./middleware/error.middleware"
-
-dotenv.config()
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-app.get("/", (req, res) => {
-  res.send("WORKING");
-});
-
-app.get("/test", (req, res) => {
-  res.send("Server working")
-})
-
-// app.use("/api/groups", groupRoutes)
-
-// app.use(errorHandler)
-
-export default app;
+// src/api/index.js
+const serverless = require("serverless-http");
+const app = require("../server");
+module.exports = serverless(app);
