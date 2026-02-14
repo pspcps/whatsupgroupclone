6c3f0c0542a0e435e0f4ec212f0ded9e3874269d
Prakash <prakash.seervi9460@gmail.com>
2026-02-14 13:28:07 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,3 @@ const app = express();
 app.get("/", (req, res) => res.send("WORKING"));
 module.exports = app;
 
-// src/api/index.js
-const serverless = require("serverless-http");
-const app = require("../server");
-module.exports = serverless(app);
